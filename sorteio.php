a09e3bda96d661adfaf02c2f05f4cc69edb239eb
BrunoErnany <Brunoernane571@gmail.com>
2024-08-29 20:13:52 -0300

    Add files via upload

---
+++ sorteio.php
@@ -0,0 +1,25 @@
+<?php
+$numero=1;
+
+switch($numero1) {
+    case 1:
+        $mensagem "uma bicicleta";
+    break;
+    case 2:
+        $mensagem "20 mil em barras de ouro";
+    break;    
+    case 3:
+        $mensagem "uma casa";
+    break;       
+    case 4:
+        $mensagem "Um conjunto de panelas";
+    break;
+    case 5: 
+        $mensagem "um carro zero";
+    break
+    default
+        $mensagem "nada infelizmente";
+    break
+}    
+echo "parabens,o seu premio foi";
+?>     
